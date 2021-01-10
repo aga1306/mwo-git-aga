26bc167ce8519f79e51e755117c489082e88753f
aga1306 <77232687+aga1306@users.noreply.github.com>
2021-01-10 15:37:46 +0100

    Update SomeProgram.java

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,9 +2,10 @@ public class SomeProgram {
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
 	  System.out.println("We will be Git experts, soon.");
+	       System.out.println("FAIL!!! do it once again, you're not an expert yet! ;/");
 	   public void manageRockets() {
     // TODO implement
 }
-	     System.out.println("FAIL!!! do it once again, you're not an expert yet! ;/");
+	  
     }
 }
